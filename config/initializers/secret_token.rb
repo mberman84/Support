283a6229337b69ae70d03c4b746ff7ade8c1670a
Matthew Berman <mberman84@gmail.com>
2011-11-20 13:25:34 -0800

    first commit - got sorcery installed and user auth done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Support::Application.config.secret_token = '9e3a7216f35ce68d12ddf746f272c4a559d302828957705cc58b720194c3156b7fecf3fb33883804a93c9be1ce6b508685c660ddebe4eab9a27c0f9097e084b9'
