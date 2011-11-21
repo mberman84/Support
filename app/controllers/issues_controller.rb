45cbb34367070af06321d146e95de7d1214f46da
Matthew Berman <mberman84@gmail.com>
2011-11-20 22:42:15 -0800

    fixing up minor links

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,13 @@
 class IssuesController < ApplicationController
   def index
-    @issues = Issue.limit(10)
+    @app_id = params[:app_id]
+    if !params[:app_id].blank?
+      @all_index = true
+      @issues = Issue.where(:app_id => params[:app_id])
+    else
+      @all_index = false
+      @issues = Issue.limit(10)
+    end
   end
   
   def show
