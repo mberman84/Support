269f984dcda75149fa4fddf13f8cf9cb9faeee1a
Matthew Berman <mberman84@gmail.com>
2011-11-22 18:55:34 -0800

    added initial validations for issues

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,13 @@ class Issue < ActiveRecord::Base
   has_many :associations, :dependent => :destroy
   has_many :users, :through => :associations
   belongs_to :app
+  
+  STATUS = ['Open', 'Closed']
+  
+  validates :subject, :presense => true,
+                      :length => { :maximum => 50 }
+  validates :description, :presence => true,
+                          :length => { :maximum => 200 }
+  validates :type, :presense => true
+  validates :status, :presense => true
 end
