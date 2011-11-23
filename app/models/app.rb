b4cf290029e4d01f3dd098cf299ace0679ca04bc
Matthew Berman <mberman84@gmail.com>
2011-11-22 18:49:50 -0800

    added some initial validations for apps

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,4 +2,13 @@ class App < ActiveRecord::Base
   belongs_to :publisher
   has_many :issues
   belongs_to :owner, class_name: "User"
+  
+  PLATFORM = ['iOS', 'Android']
+  
+  validates :name, :presence => true,
+                   :length => { :maximum => 50 }
+  validates :description, :presence => true,
+                          :length => { :maximum => 200 }
+  validates :version, :presence => true
+  validates :platform, :presence => true
 end
