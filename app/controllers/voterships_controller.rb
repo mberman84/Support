21fee77346b88eadfbd05d49f495f200e9685bea
Matthew Berman <mberman84@gmail.com>
2011-11-23 19:53:55 -0800

    added better vote up. used button_to instead of form_for

--- app/controllers/voterships_controller.rb
+++ app/controllers/voterships_controller.rb
@@ -1,7 +1,7 @@
 class VotershipsController < ApplicationController
   def create
     session[:return_to] = request.referrer
-    @issue = Issue.find(params[:votership][:issue_id])
+    @issue = Issue.find(params[:issue_id])
     @issue.cast_vote_up!(current_user.id, "up")
     redirect_to session[:return_to]
   end
