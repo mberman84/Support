59c8990869c6a9d5419c81ecf7031ddb26d32441
Matthew Berman <mberman84@gmail.com>
2011-11-24 16:55:33 -0800

    cleanup

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -15,7 +15,6 @@ class IssuesController < ApplicationController
     @app = App.find(params[:app_id])
     @issue = Issue.new(params[:issue])
     @issue.app_id = params[:app_id]
-    debugger
     if @issue.save
       flash[:success] = "Issue added!"
       redirect_to app_issue_path(@app, @issue)
