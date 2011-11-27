9961b37db89d1841f01e53afda500fc405d155a5
Matthew Berman <mberman84@gmail.com>
2011-11-26 23:50:27 -0800

    added list of highest commented issues to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    #@issues = Issue.limit(10).find(:all, :order => "")
+    @issues = Issue.find( :all, 
+                          :order => "count(comments.id) DESC", 
+                          :joins => "inner join comments on comments.issue_id = issues.id", 
+                          :group => "issues.id",
+                          :limit => "10" )
   end
 end
