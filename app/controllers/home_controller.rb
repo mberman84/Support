86f419d0f5a16aaf3422866c9aa75a3da09c3462
Matthew Berman <mberman84@gmail.com>
2011-11-26 23:53:00 -0800

    changed homepage list to vote count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   def index
+    #@issues = Issue.find( :all, 
+    #                      :order => "count(comments.id) DESC", 
+    #                      :joins => "inner join comments on comments.issue_id = issues.id", 
+    #                      :group => "issues.id",
+    #                      :limit => "10" )
     @issues = Issue.find( :all, 
-                          :order => "count(comments.id) DESC", 
-                          :joins => "inner join comments on comments.issue_id = issues.id", 
+                          :order => "count(voterships.id) DESC", 
+                          :joins => "inner join voterships on voterships.issue_id = issues.id", 
                           :group => "issues.id",
                           :limit => "10" )
   end
