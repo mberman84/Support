6bfcf5893da56e375873765c4326d3792f3bfd25
Matthew Berman <mberman84@gmail.com>
2011-12-03 11:49:22 -0800

    changed homepage to work with heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,10 +5,11 @@ class HomeController < ApplicationController
     #                      :joins => "inner join comments on comments.issue_id = issues.id", 
     #                      :group => "issues.id",
     #                      :limit => "10" )
-    @issues = Issue.find( :all, 
-                          :order => "count(voterships.id) DESC", 
-                          :joins => "left join voterships on voterships.issue_id = issues.id", 
-                          :group => "issues.id",
-                          :limit => "10" )
+    #@issues = Issue.find( :all, 
+    #                      :order => "count(voterships.id) DESC", 
+    #                      :joins => "left join voterships on voterships.issue_id = issues.id", 
+    #                      :group => "issues.id",
+    #                      :limit => "10" )
+    @issues = Issue.limit(10)
   end
 end
