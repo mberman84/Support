6e6f92d90ad89a56db45526963db211650d77fa0
Matthew Berman <mberman84@gmail.com>
2011-12-03 11:57:08 -0800

    precompile true

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
-
+    #@issues = Issue.find( :all, 
+    #                      :order => "count(comments.id) DESC", 
+    #                      :joins => "inner join comments on comments.issue_id = issues.id", 
+    #                      :group => "issues.id",
+    #                      :limit => "10" )
+    #@issues = Issue.find( :all, 
+    #                      :order => "count(voterships.id) DESC", 
+    #                      :joins => "left join voterships on voterships.issue_id = issues.id", 
+    #                      :group => "issues.id",
+    #                      :limit => "10" )
     @issues = Issue.limit(10)
   end
 end
